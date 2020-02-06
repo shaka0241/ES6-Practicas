6489b42783413f51587a4648084b2420776ce4df
Alberto Rojas <shaka0241@gmail.com>
2020-02-06 12:32:11 -0300

    dandole poco a poco

--- src/practicas.js
+++ src/practicas.js
@@ -23,7 +23,7 @@ console.log(suma);
 
 //ejemplo con el helper map
 var numbers = [2, 4, 6];
-var numberDoubled = [];
+// var numberDoubled = [];
 
 var doblados = numbers.map(function(number) {
   return number * 2;
@@ -40,7 +40,54 @@ const empresas = [
 ];
 
 var sueldos = empresas.map(function(empresa) {
-  return "En" + empresa.nombre + " mi sueldo era de: " + empresa.sueldo;
+  return "En " + empresa.nombre + " mi sueldo era de: " + empresa.sueldo;
 });
 
 console.log(sueldos);
+
+var precios = [100, 200, 300];
+
+var preciosIva = precios.map(function(precio) {
+  return (precio = precio + precio * 0.19);
+});
+
+console.log(preciosIva);
+
+var familiaRojas = [
+  { nombre: "Alberto", apellido: "Rojas" },
+  { nombre: "Jessica", apellido: "Medina" },
+  { nombre: "Carlos", apellido: "Perez" },
+  { nombre: "Raymari", apellido: "Rojas" },
+  { nombre: "Marisela", apellido: "Gallegos" }
+];
+
+var miembro = familiaRojas.filter(function(miembro) {
+  return miembro.apellido === "Rojas";
+});
+
+var miembros = miembro.map(function(miembro) {
+  return miembro.nombre + " " + miembro.apellido;
+});
+
+console.log(miembros);
+
+var productos = [
+  { nombre: "Lechuga", tipo: "verdura", cantidad: 0, precio: 11 },
+  { nombre: "Banana", tipo: "fruta", cantidad: 0, precio: 3 },
+  { nombre: "Queso", tipo: "lactosa", cantidad: 2, precio: 13 },
+  { nombre: "Tomate", tipo: "verdura", cantidad: 5, precio: 9 },
+  { nombre: "Fresa", tipo: "fruta", cantidad: 14, precio: 5 },
+  { nombre: "Cebolla", tipo: "verdura", cantidad: 1, precio: 12 }
+];
+
+var producto = productos.filter(function(producto) {
+  return (
+    producto.tipo === "verdura" && producto.cantidad > 0 && producto.precio < 10
+  );
+});
+
+var productos = producto.map(function(producto) {
+  return producto.nombre;
+});
+
+console.log(productos);
